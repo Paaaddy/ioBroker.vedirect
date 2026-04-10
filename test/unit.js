93923dcd53c5435c113c42eac9728ef33d9bafb9
Pat <pat@nanoclaw.local>
2026-04-10 19:08:12 +0200

    fix: clarify ioBroker test harness comments

--- test/unit.js
+++ test/unit.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
 
-// Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
+// Run unit tests. See https://github.com/ioBroker/testing for details and further options.
 tests.unit(path.join(__dirname, '..'), {
 
 	allowedExitCodes: [11],
